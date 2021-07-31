9a69c79701ae876b7a9ea9abd2062dabaf0c890e
AmoroBG <amorobg@gmail.com>
2021-07-31 15:11:46 -0700

    Connect the code to the UI

--- main.js
+++ main.js
@@ -1,76 +1,86 @@
 // Variables Declarations
-const humanInput = document.getElementById("game-input")
-const play = document.getElementById("play")
-const result = document.getElementById("result")
-const startGame = document.getElementById("start-game")
-let computerPlay
-let humanPlay
-let win
-    // function computerScore()
-function computerScore() {
-    // Generate Random Number
-    computerPlay = Math.floor(Math.random() * 3) + 1
-    if (computerPlay == 1) {
-        computerPlay = "Rock"
-    } else if (computerPlay == 2) {
-        computerPlay = "Paper"
-    } else {
-        computerPlay = "Scissors"
-    }
-    console.log("Computer Play: " + computerPlay);
-}
-// function humanScore() 
-function humanScore() {
+let score
+let humanChoice
+let humanScore = 0
+let computerScore = 0
+let playCount = 0
 
-    humanPlay = humanInput.value
-    if (humanPlay == 1) {
-        humanPlay = "Rock"
-    } else if (humanPlay == 2) {
-        humanPlay = "Paper"
+
+let rock = document.getElementById("rock").addEventListener("click", function() {
+    humanChoice = "Rock"
+    console.log("You Chose: " + humanChoice);
+    playGame()
+})
+let paper = document.getElementById("paper").addEventListener("click", function() {
+    humanChoice = "Paper"
+    console.log("You Chose: " + humanChoice);
+    playGame()
+})
+let scissors = document.getElementById("scissors").addEventListener("click", function() {
+    humanChoice = "Scissors"
+    console.log("You Chose: " + humanChoice);
+    playGame()
+})
+
+
+// function computerScore()
+function computerPlay() {
+    // Generate Random Number
+    computerChoice = Math.floor(Math.random() * 3) + 1
+    if (computerChoice === 1) {
+        computerChoice = "Rock"
+    } else if (computerChoice === 2) {
+        computerChoice = "Paper"
     } else {
-        humanPlay = "Scissors"
+        computerChoice = "Scissors"
     }
-    console.log("You Play: " + humanPlay);
+
+    console.log("Computer Chose: " + computerChoice);
+    return computerChoice
 }
+
 // function playGame() 
 function playGame() {
-    computerScore()
-    humanScore()
-
-    if (computerPlay == "Rock" && humanPlay == "Rock") {
-        win = "Draw"
-    } else if (computerPlay == "Rock" && humanPlay == "Paper") {
-        win = "You Won! - Computer Lost!"
-    } else if (computerPlay == "Rock" && humanPlay == "Scissors") {
-        win = "You Lost! - Computer Won!"
-    } else if (computerPlay == "Paper" && humanPlay == "Rock") {
-        win = "You Lost! - Computer Won!"
-    } else if (computerPlay == "Paper" && humanPlay == "Paper") {
-        win = "Draw!"
-    } else if (computerPlay == "Paper" && humanPlay == "Scissors") {
-        win = "You Won! - Computer Lost!"
-    } else if (computerPlay == "Scissors" && humanPlay == "Rock") {
-        win = "You Won! - Computer Lost!"
-    } else if (computerPlay == "Scissors" && humanPlay == "Paper") {
-        win = "You Lost! - Computer Won!"
-    } else if (computerPlay == "Scissors" && humanPlay == "Scissors") {
-        win = "Draw"
+    computerPlay()
+    if (computerPlay == "Rock" && humanChoice == "Rock") {
+        score = "None of you scored. Its a tie"
+        humanScore = humanScore
+        computerScore = computerScore
+    } else if (computerChoice == "Rock" && humanChoice == "Paper") {
+        score = "You Score a point!"
+        humanScore += 1
+    } else if (computerChoice == "Rock" && humanChoice == "Scissors") {
+        score = "Computer Score a point!"
+        computerScore += 1
+    } else if (computerChoice == "Paper" && humanChoice == "Rock") {
+        score = "Computer Score a point!"
+        computerScore += 1
+    } else if (computerChoice == "Paper" && humanChoice == "Paper") {
+        score = "None of you scored. Its a tie"
+        humanScore = humanScore
+        computerScore = computerScore
+    } else if (computerChoice == "Paper" && humanChoice == "Scissors") {
+        score = "You Score a point!"
+        humanScore += 1
+    } else if (computerChoice == "Scissors" && humanChoice == "Rock") {
+        score = "You Score a point!"
+        humanScore += 1
+    } else if (computerChoice == "Scissors" && humanChoice == "Paper") {
+        score = "Computer Score a point!"
+        computerScore += 1
+    } else if (computerChoice == "Scissors" && humanChoice == "Scissors") {
+        score = "None of you scored. Its a tie"
+        humanScore = humanScore
+        computerScore = computerScore
     }
-    console.log(win);
+    console.log("Score: " + score);
+    console.log("Your Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+    document.getElementById("human-score").innerText = humanScore
+    document.getElementById("computer-score").innerText = computerScore
+    document.getElementById("selection").innerHTML = score
+    document.getElementById("selection").classList.remove("display-none")
+    playCount += 1
+    document.getElementById("play-count").innerText = playCount
 }
-
-
-// Event Listeners
-play.addEventListener("click", function() {
-
-    playGame()
-    humanInput.value = ""
-    result.innerHTML = "<h2>" + win + "</h2>"
-    play.classList.add("display-none")
-    startGame.classList.remove("display-none")
-})
-startGame.addEventListener("click", function() {
-    result.innerHTML = '<input type="number" id="game-input" placeholder="Enter a number between 1 and 3 to play ...">'
-    startGame.classList.add("display-none")
-    play.classList.remove("display-none")
-})
+// playGame()
